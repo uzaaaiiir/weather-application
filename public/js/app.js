465347e3389a873c6c5967992d9d759120644eba
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-02-05 19:50:43 -0500

    Update port

--- public/js/app.js
+++ public/js/app.js
@@ -22,19 +22,17 @@ weatherForm.addEventListener("submit", (e) => {
     messageTwo.textContent = "";
     messageThree.textContent = "";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then(
-        (response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    messageOne.textContent = data.error;
-                } else {
-                    messageOne.textContent = data.location;
-                    messageTwo.textContent = `Description: ${data.weatherDescription}`;
-                    messageThree.textContent = `Temperature: ${data.temperature} (feels like ${data.feelsLike})`;
-                }
-            });
-        }
-    );
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = `Description: ${data.weatherDescription}`;
+                messageThree.textContent = `Temperature: ${data.temperature} (feels like ${data.feelsLike})`;
+            }
+        });
+    });
 
     console.log(location);
 });
